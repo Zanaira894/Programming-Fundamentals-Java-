a585aa4083933f22d0ae0d69a5b8c1de9f82c5a9
Zanaira Kanwal <zanairakanwal6870@gmail.com>
2025-11-23 23:48:47 +0500

    Add files via upload

---
+++ Java Fundamentals-I/LabActivity2.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class LabActivity2 {
+     public static void main(String[] args) {
+     Scanner console = new Scanner(System.in);
+          int length;
+          int width;
+          int area;
+          int perimeter;
+
+          System.out.print("Enter the length: ");
+          length = console.nextInt();
+
+          System.out.print("Enter the width: ");
+          width = console.nextInt();
+
+          area = length * width;
+          perimeter = 2 * (length + width);
+          System.out.println("Area = " + area);
+          System.out.println("Perimeter = " + perimeter);
+     }
+}
+    
