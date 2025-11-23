7a4fc9e62c370570a21d0ab650a39d8d0aba2de2
Zanaira Kanwal <zanairakanwal6870@gmail.com>
2025-11-23 23:52:13 +0500

    Implement currency breakdown from decimal input

---
+++ Java Fundamentals-II/ActivityA1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+public class ActivityA1{
+    public static void main (String[] args){
+       Scanner input= new Scanner(System.in);
+        System.out.println("Enter a decimal number:");
+        double amount=input.nextDouble();
+        int cents = (int)(amount*100);
+
+        int dollars= cents/100;
+        cents= cents%100;
+        int quarters= cents/25;
+        cents= cents%25;
+        int dimes= cents/10;
+        cents= cents%10;
+        int nickels= cents/5;
+        cents= cents%5;
+        int pennies= cents; 
+     
+        System.out.println("Dollars:" + dollars);
+        System.out.println("Quarters:" + quarters); 
+        System.out.println("Dimes:" + dimes);
+        System.out.println("Nickels:" + nickels);
+        System.out.println("Pennies:" + pennies);
+        
+     }
+}
+      
