c92dca414bd5077772f2e7297c724f40bfa65442
Zanaira Kanwal <zanairakanwal6870@gmail.com>
2025-11-24 00:36:23 +0500

    Rename class from lab1c to lab3 and refactor

--- Repetition control Structures/lab3.java
+++ Repetition control Structures/lab3.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class lab1c{
+public class lab3{
   public static void main(String[] args){
     Scanner input = new Scanner(System.in);
     System.out.print("Enter how many numbers are:");
@@ -14,4 +14,5 @@ public class lab1c{
      }
      System.out.print("Number of zeros" + count);
   }
+
 }
