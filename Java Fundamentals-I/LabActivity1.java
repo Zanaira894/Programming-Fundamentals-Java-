5d2acd18b93bcbb545ee019c279e0d7390e7f54d
Zanaira Kanwal <zanairakanwal6870@gmail.com>
2025-11-23 23:47:33 +0500

    Add LabActivity1 to calculate average of three numbers

---
+++ Java Fundamentals-I/LabActivity1.java
@@ -0,0 +1,16 @@
+public class LabActivity1 {
+     public static void main(String[] args) {
+          int num1;
+          int num2;
+          int num3;
+          int average;
+          num1 = 125;
+          System.out.println("Num1 = " + num1);
+          num2 = 28;
+          System.out.println("Num2 = " + num2);
+          num3 = -25;
+          System.out.println("Num3 = " + num3);
+          average = (num1 + num2 + num3)/3;
+          System.out.println("Average = " + average);
+     }
+}
