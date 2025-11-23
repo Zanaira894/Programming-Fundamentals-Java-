6b86891ed8ab3e1ad38cbe6f46dfbab1795be0f2
Zanaira Kanwal <zanairakanwal6870@gmail.com>
2025-11-24 00:53:24 +0500

    Add LabTask4 to generate random uppercase letter

---
+++ Mathematical,Characters and String Methods /LabTask4.java
@@ -0,0 +1,6 @@
+public class LabTask4 {
+    public static void main(String[] args) {
+        char ch = (char) ('A' + Math.random() * 26);
+        System.out.println("Random uppercase letter: " + ch);
+    }
+}
